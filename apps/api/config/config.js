0842033fb60e0cb8f2e98de82d69908e041ab489
Rafay-Muqeem <rafaywork93@gmail.com>
2024-12-13 15:39:54 +0500

    update config.js

--- apps/api/config/config.js
+++ apps/api/config/config.js
@@ -19,11 +19,12 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.DB_PROD_USERNAME || 'root',
-    password: process.env.DB_PROD_PASSWORD || null,
-    database: process.env.DB_PROD_NAME || 'database_production',
-    host: process.env.DB_PROD_HOST || '127.0.0.1',
-    dialect: process.env.DB_PROD_DIALECT || 'mysql',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
     logging: false,
   },
 };

a862a133742d51599b6adf447e11470c18784103
Rafay-Muqeem <rafaywork93@gmail.com>
2024-12-25 12:54:48 +0500

    update user route

--- apps/api/routes/User.js
+++ apps/api/routes/User.js
@@ -1,237 +1,255 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { User, Permission } = require('../models');
-const { body, validationResult } = require('express-validator');
-const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const fetchUser = require('../middlewares/fetchUser');
-const { Op } = require('sequelize');
-require('dotenv').config();
+const { User, Permission } = require("../models");
+const { body, validationResult } = require("express-validator");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const fetchUser = require("../middlewares/fetchUser");
+const { Op } = require("sequelize");
+require("dotenv").config();
 
-router.get('/', fetchUser, async (req, res) => {
-    try {
-        const userId = req.user.id;
-        const loggedInUser = await User.findOne({
-            where: { id: userId, role: { [Op.ne]: 'super-admin' }, BusinessId: { [Op.ne]: null } },
-        })
+router.get("/", fetchUser, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const loggedInUser = await User.findOne({
+      where: {
+        id: userId,
+        role: { [Op.ne]: "super-admin" },
+        BusinessId: { [Op.ne]: null },
+      },
+    });
 
-        if (loggedInUser) {
-            const user = await User.findAll({
-                where: { BusinessId: loggedInUser.dataValues.BusinessId },
-                include: ['Permission', 'Business']
-            });
-            return res.json(user);
-        }
-        const user = await User.findAll({
-            include: ['Permission', 'Business']
-        });
-        return res.json(user);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    if (loggedInUser) {
+      const user = await User.findAll({
+        where: { BusinessId: loggedInUser.dataValues.BusinessId },
+        include: ["Permission", "Business"],
+      });
+      return res.json(user);
     }
-})
-
-router.get('/:id', fetchUser, async (req, res) => {
-    try {
-        const user = await User.findByPk(req.params.id, {
-            include: ['Permission']
-        });
-
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        res.json(user);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
-
-router.post('/signup', [
-    body('user.email', 'Enter a valid email').isEmail(),
-    body('user.password', 'Password must be atleast 5 characters long').isLength({ min: 5 }),
-],
-fetchUser,
-async (req, res) => {
-    //Checking whether request is normal
-    const errors = validationResult(req);
-    
-    if (!errors.isEmpty()) {
-        return res.status(400).json({message: 'User not created. Enter a valid email address'})
-    }
-    
-    try {
-        console.log(req.body);
-            const userData = req.body.user;
-            
-            const existingUser = await User.findOne({
-                where: { email: userData.email}
-            });
-
-            if (existingUser) {
-                res.status(409).json({ message: 'User already exist' });
-            } else {
-                const salt = await bcryptjs.genSalt(10);
-                const secPass = await bcryptjs.hash(userData.password, salt);
-
-                const newUser = await User.create({
-                    ...userData,
-                    password: secPass
-                });
-
-                await newUser.setPermission(req.body.permissions);
-
-                res.status(200).json({ message: "User created successfully" });
-            }
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: error.message });
-        }
-    })
-
-router.post('/add_permission', async (req, res) => {
-    try {
-        const user = await User.findByPk(req.body.userId);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        if (req.body.permission.length !== 0) {
-            req.body.permissions.map(async (item) => {
-                const permission = await Permission.findByPk(item);
-                user.addPermission(permission);
-            })
-        }
-
-        res.json({ message: 'permissions added to user successfully' })
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ message: error.message });
-    }
-})
-
-router.post('/login', async (req, res) => {
-    try {
-        const user = await User.findOne({ where: { email: req.body.email } });
-
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        const passMatch = await bcryptjs.compare(req.body.password, user.password);
-
-        if (!passMatch) {
-            return res.status(404).json({ message: "Please provide correct credentials" });
-        }
-
-        const data = {
-            user: {
-                id: user.id
-            }
-        }
-
-        const token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: 12 * 60 * 60 });
-
-        res.send({ token: token, sessionExpire: Date.now() + (12 * 60 * 60 * 1000) });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error login user' });
-    }
-
-})
-
-router.post('/getuser', fetchUser, async (req, res) => {
-    try {
-        const userId = req.user.id;
-        const user = await User.findOne({
-            where: { id: userId },
-            attributes: { exclude: ['password'] },
-            include: ['Permission', 'Business']
-        });
-
-        res.send(user);
-    }
-    catch (error) {
-        console.error(error.message);
-        res.status(500).send("Internal Server Error");
-    }
+    const user = await User.findAll({
+      include: ["Permission", "Business"],
+    });
+    return res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-router.put('/update/:id', fetchUser, async (req, res) => {
+router.get("/:id", fetchUser, async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id, {
+      include: ["Permission"],
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post(
+  "/signup",
+  [
+    body("user.email", "Enter a valid email").isEmail(),
+    body(
+      "user.password",
+      "Password must be atleast 5 characters long"
+    ).isLength({ min: 5 }),
+  ],
+  fetchUser,
+  async (req, res) => {
+    //Checking whether request is normal
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ message: "User not created. Enter a valid email address" });
+    }
+
     try {
-        const user = await User.findByPk(req.params.id, {
-            include: ['Permission']
+      const userData = req.body.user;
+
+      const existingUser = await User.findOne({
+        where: { email: userData.email },
+      });
+
+      if (existingUser) {
+        res.status(409).json({ message: "User already exist" });
+      } else {
+        const salt = await bcryptjs.genSalt(10);
+        const secPass = await bcryptjs.hash(userData.password, salt);
+
+        const newUser = await User.create({
+          ...userData,
+          password: secPass,
         });
 
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
+        await newUser.setPermission(req.body.permissions);
 
-        if (req.body.user.password) {
-            const salt = await bcryptjs.genSalt(10);
-            const secPass = await bcryptjs.hash(req.body.user.password, salt);
-            req.body.user.password = secPass;
-        }
-        else {
-            req.body.user.password = user.password;
-        }
-        await user.update(req.body.user);
-
-        const deletedItems = user.Permission.filter(orgPerm =>
-            !req.body.permissions.some(item => item === orgPerm.dataValues.id))
-            .map(changeItem => changeItem.dataValues.id);
-
-        const addItems = req.body.permissions.filter(perm =>
-            !user.Permission.some(item => item.dataValues.id === perm));
-
-        if (addItems.length !== 0) {
-            addItems.forEach(async (item) => {
-                const permission = await Permission.findByPk(item);
-                await user.addPermission(permission);
-            });
-        }
-
-        if (deletedItems.length !== 0) {
-            deletedItems.forEach(async (item) => {
-                const permission = await Permission.findByPk(item);
-                await user.removePermission(permission);
-            });
-        }
-
-        res.status(200).json({ message: 'User updated successfully' });
+        res.status(200).json({ message: "User created successfully" });
+      }
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error updating user' });
+      console.error(error);
+      res.status(500).json({ message: error.message });
     }
-})
+  }
+);
 
-router.delete('/delete/:id', fetchUser, async (req, res) => {
-    try {
-        const user = await User.findByPk(req.params.id);
-
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        await user.destroy();
-        res.status(200).json({ message: 'User deleted successfully' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error deleting user' });
+router.post("/add_permission", async (req, res) => {
+  try {
+    const user = await User.findByPk(req.body.userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-})
-
-router.delete('/delete_permission', async (req, res) => {
-    try {
-        const user = await User.findByPk(req.body.userId);
-        const permission = await Permission.findByPk(req.body.permissionId);
-
-        user.removePermission(permission);
-        res.json({ message: 'permission removed from user successfully' })
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error removing permission from user' });
+    if (req.body.permission.length !== 0) {
+      await Promise.all(
+        req.body.permissions.map(async (item) => {
+          const permission = await Permission.findByPk(item);
+          await user.addPermission(permission);
+        })
+      );
     }
-})
+
+    res.json({ message: "permissions added to user successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const passMatch = await bcryptjs.compare(req.body.password, user.password);
+
+    if (!passMatch) {
+      return res
+        .status(404)
+        .json({ message: "Please provide correct credentials" });
+    }
+
+    const data = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    const token = jwt.sign(data, process.env.JWT_SECRET, {
+      expiresIn: 12 * 60 * 60,
+    });
+
+    res.send({ token: token, sessionExpire: Date.now() + 12 * 60 * 60 * 1000 });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error login user" });
+  }
+});
+
+router.post("/getuser", fetchUser, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findOne({
+      where: { id: userId },
+      attributes: { exclude: ["password"] },
+      include: ["Permission", "Business"],
+    });
+
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.put("/update/:id", fetchUser, async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id, {
+      include: ["Permission"],
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (req.body.user.password) {
+      const salt = await bcryptjs.genSalt(10);
+      const secPass = await bcryptjs.hash(req.body.user.password, salt);
+      req.body.user.password = secPass;
+    } else {
+      req.body.user.password = user.password;
+    }
+    await user.update(req.body.user);
+
+    const deletedItems = user.Permission.filter(
+      (orgPerm) =>
+        !req.body.permissions.some((item) => item === orgPerm.dataValues.id)
+    ).map((changeItem) => changeItem.dataValues.id);
+
+    const addItems = req.body.permissions.filter(
+      (perm) => !user.Permission.some((item) => item.dataValues.id === perm)
+    );
+
+    if (addItems.length !== 0) {
+      await Promise.all(
+        addItems.map(async (item) => {
+          const permission = await Permission.findByPk(item);
+          await user.addPermission(permission);
+        })
+      );
+    }
+
+    if (deletedItems.length !== 0) {
+      await Promise.all(
+        deletedItems.map(async (item) => {
+          const permission = await Permission.findByPk(item);
+          await user.removePermission(permission);
+        })
+      );
+    }
+
+    res.status(200).json({ message: "User updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error updating user" });
+  }
+});
+
+router.delete("/delete/:id", fetchUser, async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    await user.destroy();
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting user" });
+  }
+});
+
+router.delete("/delete_permission", async (req, res) => {
+  try {
+    const user = await User.findByPk(req.body.userId);
+    const permission = await Permission.findByPk(req.body.permissionId);
+
+    user.removePermission(permission);
+    res.json({ message: "permission removed from user successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error removing permission from user" });
+  }
+});
 
 module.exports = router;

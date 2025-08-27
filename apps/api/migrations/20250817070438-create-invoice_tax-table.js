7fcfefa1f6ff499c9f0b0791d1a4e679fa49bd39
Rafay-Muqeem <rafaywork93@gmail.com>
2025-08-27 22:22:45 +0500

    fix tax issue

---
+++ apps/api/migrations/20250817070438-create-invoice_tax-table.js
@@ -0,0 +1,86 @@
+"use strict";
+
+const InvoiceTaxType = require("../utils/enums/invoiceTaxType");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("invoice_tax", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      InvoiceId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "invoices",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      ProductId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "products",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      TaxId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "taxes",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      tax_name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: "",
+      },
+      tax_rate: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      tax_type: {
+        type: Sequelize.ENUM(InvoiceTaxType.Fixed, InvoiceTaxType.Percentage),
+        allowNull: false,
+        defaultValue: InvoiceTaxType.Percentage,
+      },
+      tax_amount: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: "",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("invoice_tax");
+  },
+};
